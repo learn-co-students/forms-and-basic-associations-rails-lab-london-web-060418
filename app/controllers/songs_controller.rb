e5cd985242bf19ae780c884a8449fe607cc4a266
Allegra <aukseallegra@gmail.com>
2018-07-07 19:07:17 +0100

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,22 +16,6 @@ class SongsController < ApplicationController
     redirect_to controller: 'songs', action: 'index'
   end
 
-  def edit
-    @song = Song.find(params[:id])
-  end
-
-  def update
-    @song = Song.find(params[:id])
-    @song.update(song_params)
-  end
-
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
-
   private
 
   def song_params
